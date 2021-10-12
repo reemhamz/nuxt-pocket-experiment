bc68edcdbc27e629e887f16cce6f59321acd4edb
Reem H <42309026+reemhamz@users.noreply.github.com>
2021-10-12 15:51:50 -0400

    added some pages and installed fluent-vue

---
+++ components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>This is the about page...</h1>
+    <p>
+      Well, this is just a simple about page. I'm just showcasing how some features work here on Nuxt. We're going to be using this to re-develop the pocket marketing pages.
+    </p>
+    <p>
+      Click <nuxt-link to="/">here</nuxt-link> to go back to the home page.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Hi there, I'm using Nuxt.js!</h1>
+    <h2>Click <nuxt-link to="/about">here</nuxt-link> to learn more about this project.</h2>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
