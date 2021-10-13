f50bd199b5106118827517da371e48e1890591f9
Reem H <42309026+reemhamz@users.noreply.github.com>
2021-10-13 11:29:03 -0400

    trying to get fluent to work

--- components/Landing.vue
+++ components/Landing.vue
@@ -2,15 +2,25 @@
   <header>
     <h1>Hi there, I'm using Nuxt.js!</h1>
     <h2>Click <nuxt-link to="/about">here</nuxt-link> to learn more about this project.</h2>
+    <h3>Let's check out <nuxt-link to="/fluent">Fluent in action.</nuxt-link> </h3>
   </header>
 </template>
 
+
 <script>
 export default {
-
+  methods : {},
+  data: {
+    greeting: "hello!"
+  }
 }
 </script>
 
 <style>
-
+body {
+  color: teal;
+}
+  a {
+    color: blue;
+  }
 </style>
