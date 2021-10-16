bcbb9014d68fd6e226fef536f14a73d02bb97ec0
Ivan Demchuk <ivan.demchuk@gmail.com>
2021-10-17 00:34:11 +0300

    Add nuxt build module

--- plugins/fluent-vue.js
+++ plugins/fluent-vue.js
@@ -15,8 +15,38 @@ enBundle.addResource(new FluentResource("another-key = Hello, {$name}"));
 // Create plugin istance
 // bundles - The current negotiated fallback chain of languages
 const fluent = createFluentVue({
-  bundles: [enBundle, frBundle, deBundle]
+  bundles: [enBundle]
 });
+
+const components = new Map()
+
+Vue.mixin({
+  created () {
+    components.set(this, true)
+  },
+  destroyed () {
+    components.delete(this)
+  }
+})
+
+export function changeLocale (locale) {
+  if (locale === "fr") {
+    fluent.bundles = [frBundle, enBundle]
+  }
+
+  if (locale === "de") {
+    fluent.bundles = [deBundle, enBundle]
+  }
+
+  if (locale === "en") {
+    fluent.bundles = [enBundle]
+  }
+
+  // Not sure why this is needed with nuxt 2
+  for (const component of components.keys()) {
+    component.$forceUpdate()
+  }
+}
 
 // Install Vue plugin
 Vue.use(fluent);
